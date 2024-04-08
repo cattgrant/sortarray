f14439377113ab507ab460c63f7a982ae8c50a3a
cattgrant <166399381+cattgrant@users.noreply.github.com>
2024-04-08 06:35:25 -0700

    Create arraysort.java

---
+++ arraysort.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class ArraySort {
+  public static void main(String[] args){
+  int[] array1 = {14, 1, 5, 19, 10, 21};
+
+  Arrays.sort(array1);
+  System.out.println("The sorted array is: " + Arrays.sort.toString(array1));
+  }
+}
