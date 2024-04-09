c7c7a30d27098f74af4b9c75f758a1ab31d79cc1
cattgrant <166399381+cattgrant@users.noreply.github.com>
2024-04-08 20:00:52 -0700

    Updated arraysort.java

--- arraysort.java
+++ arraysort.java
@@ -2,9 +2,30 @@ import java.util.Arrays;
 
 public class ArraySort {
   public static void main(String[] args){
-  int[] array1 = {14, 1, 5, 19, 10, 21};
+  int arraySize, num;
 
-  Arrays.sort(array1);
-  System.out.println("The sorted array is: " + Arrays.sort.toString(array1));
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter number of items to sort: ");
+  arraySize = scan.nextInt();
+
+  int[] array1 = new int[arraySize];
+
+  System.out.println("Enter numbers to sort: ");
+
+  for (int i=0; i < arraySize; i++){
+    for (int j = i+ 1; j < count; j++){
+      if (array1[i] > array1[j]){
+        num = array1[i];
+        array1[i] = array1[j];
+        array1[j] = num; 
+      }
+    }
   }
+
+  System.out.println("The sorted array is: " );
+  for (int i=0; i < arraySize; i++ ){
+    System.out.print(num[i]+ " ");
+  }
+
+}
 }
