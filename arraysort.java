79f537fb7ab6ed443eef1fd6e430e225108dbc5e
cattgrant <166399381+cattgrant@users.noreply.github.com>
2024-04-08 21:33:07 -0700

    Update arraysort.java
    -Changing array from int to handle strings
    -Error fixing on scanner

--- arraysort.java
+++ arraysort.java
@@ -2,29 +2,39 @@ import java.util.Arrays;
 
 public class ArraySort {
   public static void main(String[] args){
-  int arraySize, num;
+  int arraySize;
+  //Changed placeholder from int num to String str
+  String str;
 
   Scanner scan = new Scanner(System.in);
   System.out.println("Enter number of items to sort: ");
   arraySize = scan.nextInt();
 
-  int[] array1 = new int[arraySize];
+//Array changed to type String
+  String[] array1 = new String[arraySize];
 
-  System.out.println("Enter numbers to sort: ");
+//Changed prompt from numbers to strings
+  System.out.println("Enter strings to sort: ");
+
+//Missed entering Scanner input in main:
+  for (int i=0; i < arraySize; i++){
+    array1[i]=scan.nextLine();
+  }
+  scan.close();
 
   for (int i=0; i < arraySize; i++){
     for (int j = i+ 1; j < count; j++){
       if (array1[i] > array1[j]){
-        num = array1[i];
+        str = array1[i];
         array1[i] = array1[j];
-        array1[j] = num; 
+        array1[j] = str; 
       }
     }
   }
 
   System.out.println("The sorted array is: " );
   for (int i=0; i < arraySize; i++ ){
-    System.out.print(num[i]+ " ");
+    System.out.print(array1[i]+ " ");
   }
 
 }
